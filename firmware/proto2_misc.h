92a1bb66f33128995e5f6d34c482c735976698c3
Noah Hafner <nmh+scm2015@nomh.org>
2015-02-24 14:44:40 -1000

    split out light control to separate file

--- firmware/proto2_misc.h
+++ firmware/proto2_misc.h
@@ -4,6 +4,7 @@
 #include "avr/pgmspace.h"
 
 #include "waveforms.h"
+#include "lightingctrl.h"
 
 //#include <StandardCplusplus.h>  // included in program source
 #include <vector>
@@ -133,120 +134,6 @@ public:
     Serial.print("  (replace this callback with desired function)\n");
     delay(pause);
   }
-};
-
-// derive from thread class for HPLED
-class lightingThread: 
-public Thread
-{
-public:
-  int pin;
-  int pwr_low = 1;
-  int pwr_high = 250;
-  int LED_mode = 0;
-  int* waveform_address = (int*) &waveform_OFF;
-  int waveform_index = 0;
-  //MODES:  TODO: select a useful set of patterns
-  //0 - off
-  //1 - on, low
-  //2 - on, high
-  //3 - on, time varying (pattern/waveform)
-  //4 - same as (3) with different pattern
-  int LED_mode_lim = 7;
-
-  void run(){
-    runned();  // needed for threading infrastructure
-    //static int i = 0;
-    int val;
-    int waveform_length;
-    waveform_length = pgm_read_byte(waveform_address);
-    //use offset to skip over length in first element
-    val = pgm_read_byte(waveform_address + 1 + waveform_index); 
-    waveform_index++; 
-    waveform_index %= waveform_length;
-    analogWrite(pin, val);
-
-    //Serial.print("addr: ");
-    //Serial.print((int)waveform_address);
-    //Serial.print(", len: ");
-    //Serial.print(waveform_length);
-    //Serial.print(", i: ");
-    //Serial.print(waveform_index);
-    //Serial.print(", val: ");
-    //Serial.print(val);
-    //Serial.print("\n");
-    //Serial.print("  mode: ");
-    //Serial.print(LED_mode);
-    //Serial.print("\n");
-  }
-  void mode_cycle(){
-    LED_mode += 1;
-    LED_mode %= LED_mode_lim;
-
-    waveform_index = 0;  // reset to start of waveform on change
-
-    if(0 == LED_mode){
-      waveform_address = (int*) &waveform_OFF;
-    }
-    else if(1 == LED_mode){
-      waveform_address = (int*) &waveform_LOW;
-    }
-    else if(2 == LED_mode){
-      waveform_address = (int*) &waveform_FLASH_LOW;
-    }
-    else if(3 == LED_mode){
-      waveform_address = (int*) &waveform_BEACON_LINEAR;
-    }
-    else if(4 == LED_mode){
-      waveform_address = (int*) &waveform_BEACON_SINUSOIDAL;
-    }
-    else if(5 == LED_mode){
-      waveform_address = (int*) &waveform_PULSE_LONG;
-    }
-    else if(6 == LED_mode){
-      waveform_address = (int*) &waveform_HIGH;
-    }
-  }
-
-  void OLDrun(){
-    runned();  // needed for threading infrastructure
-//    static int i = 0;
-//  //  if(0 > LED_mode){
-//  //    LED_mode = 0;
-//  //  }
-//  //  if(LED_mode > LED_mode_lim){
-//  //    LED_mode = LED_mode_lim - 1;
-//  //  }
-//    // MODE PROGRAMMING
-//    if(0 == LED_mode){
-//      analogWrite(pin, 0);
-//    }
-//    else if(1 == LED_mode){
-//      analogWrite(pin, pwr_low);
-//    }
-//    else if(2 == LED_mode){
-//      i++;
-//      i %= wfrmA_len;
-//      analogWrite(pin, wfrmA[i]);
-//    }
-//    else if(3 == LED_mode){
-//      i++;
-//      i %= wfrmB_len;
-//      analogWrite(pin, wfrmB[i]);
-//    }
-//    else if(4 == LED_mode){
-//      i++;
-//      i %= wfrmC_len;
-//      analogWrite(pin, wfrmC[i]);
-//    }
-//    else if(5 == LED_mode){
-//      analogWrite(pin, pwr_high);
-//    }
-  }
-//  void OLDmode_cycle(){
-//    LED_mode += 1;
-//    LED_mode %= LED_mode_lim;
-//  }
 };
 
 #endif //PROTO2_MISC_H
