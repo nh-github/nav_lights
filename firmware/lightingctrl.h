bc85c4475100e7a071b542e6f25606176ef3c332
Noah Hafner <nmh+scm2015@nomh.org>
2015-02-25 22:44:27 -1000

    testing macro defined waveforms

--- firmware/lightingctrl.h
+++ firmware/lightingctrl.h
@@ -13,6 +13,14 @@
 typedef void (*cb)(int); 
 
 
+//LightingThread::LightingThread(int output_pin):
+//m_output_pin(output_pin),
+//m_LED_mode(0),
+//m_waveform_index(0)
+//{
+//}
+
+
 // derive from thread class for HPLED
 class lightingThread: 
 public Thread
@@ -24,6 +32,9 @@ public:
   int LED_mode = 0;
   int* waveform_address = (int*) &waveform_OFF;
   int waveform_index = 0;
+  int m_output_pin;
+  int m_LED_mode = 0;
+  int m_waveform_index = 0;
   //MODES:  TODO: select a useful set of patterns
   //0 - off
   //1 - on, low
@@ -35,15 +46,24 @@ public:
   void run(){
     runned();  // needed for threading infrastructure
     //static int i = 0;
-    int val;
-    int waveform_length;
-    waveform_length = pgm_read_byte(waveform_address);
+    //int val;
+    //int waveform_length;
+    //waveform_length = pgm_read_byte(waveform_address);
     //use offset to skip over length in first element
-    val = pgm_read_byte(waveform_address + 1 + waveform_index); 
-    waveform_index++; 
-    waveform_index %= waveform_length;
-    analogWrite(pin, val);
-
+    //val = pgm_read_byte(waveform_address + 1 + waveform_index); 
+    //waveform_index++; 
+    //waveform_index %= waveform_length;
+    //analogWrite(pin, val);
+///////////////////////////////////////////
+    const int waveform_len = waveforms_len[m_LED_mode];
+    const int* waveform_ptr = waveforms_table[m_LED_mode];
+    waveform_ptr += m_waveform_index % waveform_len;
+    int val = pgm_read_byte(waveform_ptr);
+    // move to next sample for next time around
+    m_waveform_index++;
+    // write the value to the pin
+    analogWrite(m_output_pin, val);
+///////////////////////////////////////////
     //Serial.print("addr: ");
     //Serial.print((int)waveform_address);
     //Serial.print(", len: ");
@@ -58,32 +78,37 @@ public:
     //Serial.print("\n");
   }
   void mode_cycle(){
-    LED_mode += 1;
-    LED_mode %= LED_mode_lim;
+    // increment LED mode and wrap around
+    m_LED_mode = (m_LED_mode + 1) % int(WAVEFORM_COUNT_E);
+    // reset to start of waveform on change
+    m_waveform_index = 0;
 
-    waveform_index = 0;  // reset to start of waveform on change
+    //LED_mode += 1;
+    //LED_mode %= LED_mode_lim;
 
-    if(0 == LED_mode){
-      waveform_address = (int*) &waveform_OFF;
-    }
-    else if(1 == LED_mode){
-      waveform_address = (int*) &waveform_LOW;
-    }
-    else if(2 == LED_mode){
-      waveform_address = (int*) &waveform_FLASH_LOW;
-    }
-    else if(3 == LED_mode){
-      waveform_address = (int*) &waveform_BEACON_LINEAR;
-    }
-    else if(4 == LED_mode){
-      waveform_address = (int*) &waveform_BEACON_SINUSOIDAL;
-    }
-    else if(5 == LED_mode){
-      waveform_address = (int*) &waveform_PULSE_LONG;
-    }
-    else if(6 == LED_mode){
-      waveform_address = (int*) &waveform_HIGH;
-    }
+    //waveform_index = 0;  // reset to start of waveform on change
+
+    //if(0 == LED_mode){
+    //  waveform_address = (int*) &waveform_OFF;
+    //}
+    //else if(1 == LED_mode){
+    //  waveform_address = (int*) &waveform_LOW;
+    //}
+    //else if(2 == LED_mode){
+    //  waveform_address = (int*) &waveform_FLASH_LOW;
+    //}
+    //else if(3 == LED_mode){
+    //  waveform_address = (int*) &waveform_BEACON_LINEAR;
+    //}
+    //else if(4 == LED_mode){
+    //  waveform_address = (int*) &waveform_BEACON_SINUSOIDAL;
+    //}
+    //else if(5 == LED_mode){
+    //  waveform_address = (int*) &waveform_PULSE_LONG;
+    //}
+    //else if(6 == LED_mode){
+    //  waveform_address = (int*) &waveform_HIGH;
+    //}
   }
 
 };
--- firmware/waveforms.h
+++ firmware/waveforms.h
@@ -4,52 +4,74 @@
 #include "avr/pgmspace.h"
 
 // waveform storage bits
-// store length in first element
+#define DECLARE_WAVEFORMS(DECLARE_WAVEFORM)     \
+  DECLARE_WAVEFORM(OFF, 0)                      \
+  DECLARE_WAVEFORM(LOW, 1, 1, )                 \
+  DECLARE_WAVEFORM(HIGH, 255)                   \
+  DECLARE_WAVEFORM(FLASH_LOW,                   \
+                   1,  1,  0,  0,  0,  0,  0,  0,  0,  0, \
+                   0,  0,  0,  0,  0,  0,  0,  0,  0,  0, \
+                   1,  0,  0,  0,  0,  0,  1,  0,  0,  0, \
+                   0,  0,  0,  0,  0,  0,  0,  0,  0,  0, \
+                   0,  0,  0,  0,  0,  0,  0,  0,  0,  0, )   \
+                                                              \
+  DECLARE_WAVEFORM(BEACON_LINEAR,                             \
+                   1, 1<<2, 1<<3, 1<<4, 1<<5, 1<<6, 1<<7, 1<<7, 1<<7, 1<<7, \
+                   1<<7, 1<<7, 1<<7, 1<<7, 1<<6, 1<<5, 1<<4, 1<<3, 1<<2, 1, \
+                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                        \
+                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                        \
+                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1)                        \
+                                                                        \
+  DECLARE_WAVEFORM(BEACON_SINUSOIDAL,                                   \
+                   1, 4, 16, 34, 58, 87, 118, 149, 179, 205,            \
+                   227, 242, 250, 250, 242, 227, 205, 179, 149, 118,    \
+                   87, 58, 34, 16, 4, 1, 1, 1, 1, 1,                    \
+                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                        \
+                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1)                        \
+                                                                        \
+  DECLARE_WAVEFORM(PULSE_LONG,                                          \
+                   0, 0, 0, 1, 1, 2, 2, 3, 4, 5,                        \
+                   6, 7, 9, 10, 12, 13, 15, 17, 18, 20,                 \
+                   22, 24, 26, 28, 30, 32, 34, 36, 38, 40,              \
+                   42, 44, 46, 47, 49, 51, 52, 54, 55, 57,              \
+                   58, 59, 60, 61, 62, 62, 63, 63, 64, 64,              \
+                   64, 64, 64, 63, 63, 62, 62, 61, 60, 59,              \
+                   58, 57, 55, 54, 52, 51, 49, 47, 46, 44,              \
+                   42, 40, 38, 36, 34, 32, 30, 28, 26, 24,              \
+                   22, 20, 18, 17, 15, 13, 12, 10, 9, 7,                \
+                   6, 5, 4, 3, 2, 2, 1, 1, 0, 0)
 
-// off
-const PROGMEM int waveform_OFF[] = { 1, 0, };
 
-// on (low)
-const PROGMEM int waveform_LOW[] = { 2, 1, 1, };
 
-// on (high)
-const PROGMEM int waveform_HIGH[] = { 1, 255, };
 
-// low power flashing
-const PROGMEM int waveform_FLASH_LOW[] = { 50, // 50 elements
-    1,  1,  0,  0,  0,  0,  0,  0,  0,  0, 
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    1,  0,  0,  0,  0,  0,  1,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0, };
+#define DECL_Waveforms_e_ENTRY(name, ...) WAVEFORM_ ## name ## _E,
 
-// linear ramp flash
-const PROGMEM int waveform_BEACON_LINEAR[] = { 50, // 50 elements
-    1, 1<<2, 1<<3, 1<<4, 1<<5, 1<<6, 1<<7, 1<<7, 1<<7, 1<<7,
-    1<<7, 1<<7, 1<<7, 1<<7, 1<<6, 1<<5, 1<<4, 1<<3, 1<<2, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
+enum Waveforms_e {
+  DECLARE_WAVEFORMS(DECL_Waveforms_e_ENTRY)
+  WAVEFORM_COUNT_E,
+};
 
-// sinusoidal ramp flash
-const PROGMEM int waveform_BEACON_SINUSOIDAL[] = { 50, // 50 elements
-    1, 4, 16, 34, 58, 87, 118, 149, 179, 205,
-    227, 242, 250, 250, 242, 227, 205, 179, 149, 118,
-    87, 58, 34, 16, 4, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
 
-// longer pulsing [?]
-const PROGMEM int waveform_PULSE_LONG[] = { 100, // 100 elements
-    0, 0, 0, 1, 1, 2, 2, 3, 4, 5,
-    6, 7, 9, 10, 12, 13, 15, 17, 18, 20,
-    22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
-    42, 44, 46, 47, 49, 51, 52, 54, 55, 57,
-    58, 59, 60, 61, 62, 62, 63, 63, 64, 64,
-    64, 64, 64, 63, 63, 62, 62, 61, 60, 59,
-    58, 57, 55, 54, 52, 51, 49, 47, 46, 44,
-    42, 40, 38, 36, 34, 32, 30, 28, 26, 24,
-    22, 20, 18, 17, 15, 13, 12, 10, 9, 7,
-    6, 5, 4, 3, 2, 2, 1, 1, 0, 0  };
+#define DECL_WAVEFORM_LEN_ENTRY(name, ...) (sizeof((int[]){__VA_ARGS__})/sizeof(int)),
+
+const int waveforms_len[WAVEFORM_COUNT_E] = {
+  DECLARE_WAVEFORMS(DECL_WAVEFORM_LEN_ENTRY)
+};
+
+
+#define DECL_waveform_VARNAME(VARNAME, ...) waveform_ ## VARNAME
+
+
+#define DECL_waveform_(name, ...)                                     \
+  const PROGMEM int DECL_waveform_VARNAME(name) [] = { __VA_ARGS__ };
+
+DECLARE_WAVEFORMS(DECL_waveform_);
+
+
+#define DECL_waveform_table_ENTRY(VARNAME, ...) waveform_ ## VARNAME,
+
+const int* waveforms_table[WAVEFORM_COUNT_E] = {
+  DECLARE_WAVEFORMS(DECL_waveform_table_ENTRY)
+};
 
 #endif //WAVEFORMS_H
