114a9a5f682be2ca047169e34ebd3bf2943448fa
Noah Hafner <nmh+scm2015@nomh.org>
2015-03-05 22:29:29 -1000

    Update macro waveform definitions
    clean data, reorder waveforms, and update to latest version (McKay)

--- firmware/waveforms.h
+++ firmware/waveforms.h
@@ -4,10 +4,12 @@
 #include "avr/pgmspace.h"
 
 // waveform storage bits
+
+// macro to declare all waveforms
+// applies the DECLARE_WAVEFORM macro to each waveform definition
 #define DECLARE_WAVEFORMS(DECLARE_WAVEFORM)     \
   DECLARE_WAVEFORM(OFF, 0)                      \
-  DECLARE_WAVEFORM(LOW, 1, 1, )                 \
-  DECLARE_WAVEFORM(HIGH, 255)                   \
+  DECLARE_WAVEFORM(LOW, 1)                      \
   DECLARE_WAVEFORM(FLASH_LOW,                   \
                    1,  1,  0,  0,  0,  0,  0,  0,  0,  0, \
                    0,  0,  0,  0,  0,  0,  0,  0,  0,  0, \
@@ -39,39 +41,51 @@
                    58, 57, 55, 54, 52, 51, 49, 47, 46, 44,              \
                    42, 40, 38, 36, 34, 32, 30, 28, 26, 24,              \
                    22, 20, 18, 17, 15, 13, 12, 10, 9, 7,                \
-                   6, 5, 4, 3, 2, 2, 1, 1, 0, 0)
+                   6, 5, 4, 3, 2, 2, 1, 1, 0, 0)                        \
+  DECLARE_WAVEFORM(HIGH, 255)                                           \
+  //DECLARE_WAVEFORM(NAME, values...)                                   \
 
 
 
-
+// This macro expands to create the enum def
 #define DECL_Waveforms_e_ENTRY(name, ...) WAVEFORM_ ## name ## _E,
 
+// Declare the Waveforms_e enum type
 enum Waveforms_e {
+  // For each waveform, expand the macro to declare the enum value
   DECLARE_WAVEFORMS(DECL_Waveforms_e_ENTRY)
   WAVEFORM_COUNT_E,
 };
 
 
+// This macro expands to create the waveform_len array entry value
 #define DECL_WAVEFORM_LEN_ENTRY(name, ...) (sizeof((int[]){__VA_ARGS__})/sizeof(int)),
 
+// Declare the waveforms_len array
 const int waveforms_len[WAVEFORM_COUNT_E] = {
+  // Foe each waveform, expand the macro to compute the waveform array length
   DECLARE_WAVEFORMS(DECL_WAVEFORM_LEN_ENTRY)
 };
 
 
+// This macro expands to the unique waveform array name
 #define DECL_waveform_VARNAME(VARNAME, ...) waveform_ ## VARNAME
 
 
+// This macro expands to declare and initialize the waveform array variable
 #define DECL_waveform_(name, ...)                                     \
   const PROGMEM int DECL_waveform_VARNAME(name) [] = { __VA_ARGS__ };
 
+// For each waveform, expand the macro to declare and initialize the waveform array variable
 DECLARE_WAVEFORMS(DECL_waveform_);
 
 
-#define DECL_waveform_table_ENTRY(VARNAME, ...) waveform_ ## VARNAME,
+// This macro expands to the waveform array variable name with a trailing comma
+#define DECL_waveform_table_ENTRY(name, ...) DECL_waveform_VARNAME(name),
 
+
+// Declare the waveforms_table variable initialized for each waveform array variable
 const int* waveforms_table[WAVEFORM_COUNT_E] = {
   DECLARE_WAVEFORMS(DECL_waveform_table_ENTRY)
 };
-
 #endif //WAVEFORMS_H
